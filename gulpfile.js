b954cc5f3e3a1ef24b4973f56b9f6f53f22c423e
Jorge Mendoza <mencor.corp@gmail.com>
2023-02-24 13:59:05 -0600

    varias tareas

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 const gulp = require('gulp');
-
+const server = require('gulp-server-livereload');
 
 
 gulp.task('build',(cb) => {
     console.log("Contruyendo...")
     setTimeout(cb,1200);
-}) 
+}); 
+
+
+gulp.task('serve',(cb) => {
+    gulp.src('www')
+    .pipe(server({
+        livereload: true,
+        open: true,
+    }));
+});
+
+
+gulp.task('default',gulp.series('build','serve'));
